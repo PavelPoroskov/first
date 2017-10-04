24078514c5f997bc241bed4096af605b9cbcf9a4
pavl1853 <pavl1853@gmail.com>
2017-10-05 02:26:14 +0300

    cdn

---
+++ wp-cdn-es6/src/index-react-app.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Welcome1 extends React.Component {
+  render() {
+    return <h1>Hello, {this.props.name}</h1>;
+  }
+}
+
+//const Welcome2 = ({name}) => <h1>Hello, {name}</h1>;
+class Welcome2 extends React.Component {
+  render() {
+    return <h1>Hello, {this.props.name}</h1>;
+  }
+}
+
+ReactDOM.render([
+  <Welcome1 name="Sara1" />,
+  <Welcome2 name="Sara2" />
+  ],
+  document.getElementById('root')
+);
+
---
+++ wp-cdn-es6/webpack.config.full.js
@@ -0,0 +1,55 @@
+//const CleanWebpackPlugin = require('clean-webpack-plugin'); 
+const Dotenv = require('dotenv-webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
+
+//const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+  entry: {
+//  	env: './src/index-env.js'
+    appfull: './src/index-react-app.js'
+  },
+  // devtool:'inline-source-map', // no production
+  // devServer:{                  // no production
+  //   contentBase:'./build',       // no production
+  // },                           // no production
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'build')
+//    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+//              ["env", { "targets": { "browsers": "last 2 versions", "uglify": true } }],
+              ["env", { "targets": { "uglify": true } }],
+              'react'
+              ]
+          }
+        }
+      }
+    ]
+  }, 
+  plugins: [
+//    new CleanWebpackPlugin(['build']),
+    new HtmlWebpackPlugin({template: './src/index.html'}),
+    new Dotenv(),
+    // new webpack.DefinePlugin({
+    //   "process.env.NODE_ENV": JSON.stringify("production")
+    // }),
+    new webpack.optimize.UglifyJsPlugin()
+      // ({
+      // uglifyOptions: {
+      //   ecma: 6
+      // } })
+//    new UglifyJSPlugin()
+  ]  
+};
