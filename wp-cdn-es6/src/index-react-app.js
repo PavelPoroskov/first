24947d6aab9264f2936ca5c78fc37ab5b41a38c9
pavl1853 <pavl1853@gmail.com>
2017-10-07 22:28:44 +0300

    m

--- wp-cdn-es6/src/index-react-app.js
+++ wp-cdn-es6/src/index-react-app.js
@@ -7,12 +7,12 @@ class Welcome1 extends React.Component {
   }
 }
 
-//const Welcome2 = ({name}) => <h1>Hello, {name}</h1>;
-class Welcome2 extends React.Component {
-  render() {
-    return <h1>Hello, {this.props.name}</h1>;
-  }
-}
+const Welcome2 = ({name}) => <h1>Hello, {name}</h1>;
+//class Welcome2 extends React.Component {
+//  render() {
+//    return <h1>Hello, {this.props.name}</h1>;
+//  }
+//}
 
 ReactDOM.render([
   <Welcome1 name="Sara1" />,
