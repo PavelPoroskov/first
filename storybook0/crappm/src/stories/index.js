61e428a5ef0ff0633139c0ed1560fb1bbff5ca40
pavl1853 <pavl1853@gmail.com>
2018-01-19 22:53:37 +0300

    try storybook

---
+++ storybook0/crappm/src/stories/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import Button from '../components/Button';
+
+storiesOf('button', module)
+  .add('with text', () => (
+    <button onClick={action('clicked')}>Hello Button</button>
+  ))
+  .add('with some emoji', () => (
+    <button onClick={action('clicked')}>😀 😎 👍 💯</button>
+  )); 
+
+storiesOf('Button', module)
+  .add('with text', () => (
+    <Button onClick={action('clicked')}>Hello Button</Button>
+  ))
+  .add('with some emoji', () => (
+    <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>
+  )); 
