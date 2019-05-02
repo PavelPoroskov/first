1c30e1c1a86bf3faad781c1507887866f899cde6
PavelPoroskov <pavl1853@gmail.com>
2019-05-02 16:35:21 +0300

    fix vulnerability

--- microservice/index.js
+++ microservice/index.js
@@ -1,21 +1,25 @@
-const { send } = require('micro')
-const url = require('url')
-const level = require('level')
-const promisify = require('then-levelup')
+const { send } = require("micro");
+const url = require("url");
+const level = require("level");
 
-const db = promisify(level('visits.db', {
-  valueEncoding: 'json'
-}))
+const db = level("visits.db", {
+  valueEncoding: "json"
+});
 
-module.exports = async function (request, response) {
-  const { pathname } = url.parse(request.url)
+module.exports = async function(request, response) {
+  const { pathname } = url.parse(request.url);
 
+  console.log(`pathname ${pathname}`);
   try {
-    const currentVisits = await db.get(pathname)
-    await db.put(pathname, currentVisits + 1)
+    const currentVisits = await db.get(pathname);
+    // console.log(`currentVisits ${currentVisits}`)
+    await db.put(pathname, currentVisits + 1);
+    // console.log(`after put}`)
   } catch (error) {
-    if (error.notFound) await db.put(pathname, 1)
+    // console.log(`catch (error)`)
+    // console.log(error)
+    if (error.notFound) await db.put(pathname, 1);
   }
 
-  send(response, 200, `This page has ${await db.get(pathname)} visits!`)
-}
+  send(response, 200, `This page has ${await db.get(pathname)} visits!`);
+};
