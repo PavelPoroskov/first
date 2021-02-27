12133e4cf024c84c747b1c51cc8712ff63cc97cd
Pavel Poroskov <pavel.poroskov.se@gmail.com>
2021-02-28 01:04:15 +0300

    use cjs and es6 modules, await import('es6')

--- nodejs/esmodules/consumer-cjs/index.js
+++ nodejs/esmodules/consumer-cjs/index.js
@@ -5,6 +5,25 @@ const { hello } = require('hello-cjs');
 // not work
 // const { helloES6 } = require('./proxy.mjs');
 
+// work
+// import('./proxy.mjs').then(module => module.helloES6());
+
+// work
+// import('hello-es6').then(module => module.hello());
+
 hello();
 
 // helloES6();
+
+const test = async () => {
+  let helloES6;
+
+  await import('hello-es6').then(module => {
+    helloES6 = module.hello;
+  });
+
+  hello();
+  helloES6();
+};
+
+test();
